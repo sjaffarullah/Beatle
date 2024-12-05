9f222b76e144bdcf4f9aa8d378d6c2f606ac127e
125998211-myseneca <sjaffarullah@myseneca.ca>
2024-12-05 18:46:16 -0500

    Added code for IAM roles and policy creation

--- main.tf
+++ main.tf
@@ -46,4 +46,49 @@ resource "aws_dynamodb_table" "music_metadata" {
   }
 }
 
+# IAM Role for Lambda
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda_exec_role_beatle"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Policies to IAM Role
+resource "aws_iam_role_policy_attachment" "lambda_dynamodb_access" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_s3_access" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_logs_access" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/CloudWatchLogsFullAccess"
+}
+
+# Lambda Function Definition
+resource "aws_lambda_function" "get_tracks" {
+  function_name = "GetTracks"
+  role          = aws_iam_role.lambda_exec.arn
+  handler       = "lambda_function.handler"
+  runtime       = "python3.9"
+
+  filename         = "lambda_function.zip" # this zip contains lambda function codes
+  source_code_hash = filebase64sha256("lambda_function.zip") 
+}
+
 
